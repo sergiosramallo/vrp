33f93c88e6905c92b9104b56fb38469b66316c88
sergiosramallo <sergiosramallo@gmail.com>
2026-05-20 20:11:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ def geocodificar(direccion):
 
 # Carga de archivos
 st.sidebar.header("Carga de Datos")
-file1 = st.sidebar.file_uploader("Subir Hoja 1", type=["csv"])
-file2 = st.sidebar.file_uploader("Subir Hoja 2", type=["csv"])
+file1 = st.sidebar.file_uploader("Subir Hoja 1", type=["xlsx", "xls"])
+file2 = st.sidebar.file_uploader("Subir Hoja 2", type=["xlsx", "xls"])
 
 if file1 and file2:
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
+    df1 = pd.read_excel(file1)
+    df2 = pd.read_excel(file2)
     df = pd.concat([df1, df2])
     
     st.subheader("Vista previa de Sucursales")
